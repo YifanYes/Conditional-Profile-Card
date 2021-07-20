09ae22a5343624c4c90854db06810ec5a68d8a7b
Yifan <yifanyemontpe@gmail.com>
2021-07-20 13:37:58 +0000

    Added exercise files

--- src/js/index.js
+++ src/js/index.js
@@ -33,14 +33,14 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${variables.name}${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}"> 
+            <li><a href=" "><i class="fa fa-twitter">${variables.twitter}</i></a></li>
+            <li><a href=" "><i class="fa fa-github">${variables.github}</i></a></li>
+            <li><a href=" "><i class="fa fa-linkedin">${variables.linkedin}</i></a></li>
+            <li><a href=" "><i class="fa fa-instagram">${variables.instagram}</i></a></li>
           </ul>
         </div>
     `;
